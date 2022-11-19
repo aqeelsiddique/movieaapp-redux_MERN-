08857cdc02b221639b5ae65b9ab0591d8c4b4768
Aqeel <aqeelsaddique83@gmail.com>
2022-11-20 00:50:53 +0500

    getmovies

--- server/controllers/movie_controller.js
+++ server/controllers/movie_controller.js
@@ -141,6 +141,19 @@ getMovies = async (req, res) => {
         return res.status(200).json({ success: true, data: movies })
     }).catch(err => console.log(err))
 }
+getMovies = async (req, res) => {
+    await Movie.find({}, (err, movies) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!movies.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+        return res.status(200).json({ success: true, data: movies })
+    }).catch(err => console.log(err))
+}
 
 module.exports = {
     createMovie,
